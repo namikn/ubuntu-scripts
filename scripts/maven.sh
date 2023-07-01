ca79e8661d3a6d3169b3bce56a0f5b4077686193
Namik Nuhanović <namik.nuhanovic@gmail.com>
2023-07-01 16:51:24 +0200

    Upgrade Maven version

--- scripts/maven.sh
+++ scripts/maven.sh
@@ -1,12 +1,12 @@
 # Maven
 # https://maven.apache.org/install.html
 
-_VERSION=3.9.2
+_VERSION=3.9.3
 
 _URL=https://downloads.apache.org/maven/maven-3/$_VERSION/binaries/apache-maven-$_VERSION-bin.tar.gz
 
 echo $_URL
 curl -sSL $_URL -o apache-maven.tar.gz
 sudo tar -xf apache-maven.tar.gz -C /opt
-sudo ln -s /opt/apache-maven-$_VERSION/bin/mvn /usr/local/bin/mvn
+sudo ln -sf /opt/apache-maven-$_VERSION/bin/mvn /usr/local/bin/mvn
 rm -f apache-maven.tar.gz
